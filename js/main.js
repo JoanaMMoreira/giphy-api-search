7867e55a63ba6c0fe6cd2efeff04b58fa8f2dc34
Joana Moreira <joanamoreira989@gmail.com>
2019-02-04 11:04:12 +0000

    Added loop to replace variables

--- js/main.js
+++ js/main.js
@@ -32,26 +32,11 @@ $(document).ready(function () {
     httpsGetAsync('https://api.giphy.com/v1/gifs/search?' + params, function (data) {
       var gifs = JSON.parse(data);
 
-      var gif1 = gifs.data[0].images.fixed_width.url;
-      $(".pos-1").html("<a href='" + gifs.data[0].embed_url + "'><img src='" + gif1 + "' alt='gif'></a>");
-
-      var gif2 = gifs.data[1].images.fixed_width.url;
-      $(".pos-2").html("<a href='" + gifs.data[1].embed_url + "'><img src='" + gif2 + "' alt='gif'></a>");
-
-      var gif3 = gifs.data[2].images.fixed_width.url;
-      $(".pos-3").html("<a href='" + gifs.data[2].embed_url + "'><img src='" + gif3 + "' alt='gif'></a>");
-
-      var gif4 = gifs.data[3].images.fixed_width.url;
-      $(".pos-4").html("<a href='" + gifs.data[3].embed_url + "'><img src='" + gif4 + "' alt='gif'></a>");
-
-      var gif5 = gifs.data[4].images.fixed_width.url;
-      $(".pos-5").html("<a href='" + gifs.data[4].embed_url + "'><img src='" + gif5 + "' alt='gif'></a>");
-
-      var gif6 = gifs.data[5].images.fixed_width.url;
-      $(".pos-6").html("<a href='" + gifs.data[5].embed_url + "'><img src='" + gif6 + "' alt='gif'></a>");
-
-      var gif7 = gifs.data[6].images.fixed_width.url;
-      $(".pos-7").html("<a href='" + gifs.data[6].embed_url + "'><img src='" + gif7 + "' alt='gif'></a>");
+      for (i = 0; i < gifs.data.length; i++) {
+        var gif = gifs.data[i].images.fixed_width.url;
+        var position = $(".pos-" + (i + 1));
+        position.html("<a href='" + gifs.data[0].embed_url + "'><img src='" + gif + "' alt='gif'></a>");
+      }
 
       console.log(gifs.data);
     });
